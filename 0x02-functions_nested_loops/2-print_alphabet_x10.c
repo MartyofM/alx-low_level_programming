2946b8d03cddaa0330d62e9da7530df81a185caf
Mathew <mwangimathewkariuki@gmail.com>
2023-02-21 09:21:36 -0800

    alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,19 @@
 /**
  * print_alphabet_x10 - Make alphabet x10 times
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char s;
 	int i = 0;
 
 	while (i <= 9)
+	{
 	for (s = 'a'; s <= 'z'; s++)
 	{
 	_putchar(s);
-
+	}
 	_putchar('\n');
 	i++;
 	}

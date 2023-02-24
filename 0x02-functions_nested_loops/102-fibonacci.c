de87d7dfe7996d3a42af4d1c19cda96b4c23ad9e
Mathew <mwangimathewkariuki@gmail.com>
2023-02-24 08:51:49 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 52 fibonacci numbers
+ * Return: Nothing!
+ */
+
+int main(void)
+
+{
+	int s = 0;
+	long t = 1, r = 2;
+
+	while (s < 50)
+	{
+	if (s == 0)
+		printf("%ld", t);
+	else if (s == 1)
+		printf("%ld", r);
+	else
+	{
+		r += t;
+		t = r - t;
+		printf(", %ld", r);
+	}
+	++s;
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci numbers
+ * less than 4000000.
+ * Return: Nothing!
+ */
+
+int main(void)
+
+{
+	int m = 0;
+	long n = 1, p = 2, sum = p;
+
+	while (p + n < 4000000)
+	{
+		p += n;
+	if ( p % 2 == 0)
+	sum += p;
+	n = p - n;
+	++m;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
